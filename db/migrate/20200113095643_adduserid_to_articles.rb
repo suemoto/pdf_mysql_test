64621fb0ca672d603c8f106e292f2329237f436b
suemoto <s_bdash_0802@yahoo.co.jp>
2020-01-13 19:09:57 +0900

    edit_sign-in_link
    サインアップ、サインインのリンクをログイン状態での表示が変わるように修正。記事を作成したユーザーだけ記事を編集、削除できるようにボタンの表示を修正

---
+++ db/migrate/20200113095643_adduserid_to_articles.rb
@@ -0,0 +1,5 @@
+class AdduseridToArticles < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :myfiles, :user, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_13_093800) do
+ActiveRecord::Schema.define(version: 2020_01_13_095643) do
 
   create_table "myfiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2020_01_13_093800) do
     t.string "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_myfiles_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -33,4 +35,5 @@ ActiveRecord::Schema.define(version: 2020_01_13_093800) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "myfiles", "users"
 end
