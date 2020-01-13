56fe3ba20ca97986ee9238a5ff9d40dc1643bd4e
suemoto <s_bdash_0802@yahoo.co.jp>
2020-01-13 20:37:00 +0900

    setting_foreign_key
    外部キーの制約を編集中

--- db/migrate/20200113095643_adduserid_to_articles.rb
+++
@@ -1,5 +0,0 @@
-class AdduseridToArticles < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :myfiles, :user, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_13_095643) do
+ActiveRecord::Schema.define(version: 2020_01_13_110643) do
 
   create_table "myfiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_01_13_095643) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -30,10 +31,8 @@ ActiveRecord::Schema.define(version: 2020_01_13_095643) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "myfiles", "users"
 end
