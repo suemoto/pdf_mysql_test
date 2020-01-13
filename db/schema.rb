742a46f6d4ea714146c612c8d8e8e158bfc35399
suemoto <s_bdash_0802@yahoo.co.jp>
2020-01-13 21:05:10 +0900

    setting_references
    外部キーの調整中

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_13_110643) do
+ActiveRecord::Schema.define(version: 2020_01_13_092740) do
 
   create_table "myfiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
     t.string "filename"
     t.string "comment"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
     t.index ["user_id"], name: "index_myfiles_on_user_id"
   end
 
@@ -35,4 +35,5 @@ ActiveRecord::Schema.define(version: 2020_01_13_110643) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "myfiles", "users"
 end
