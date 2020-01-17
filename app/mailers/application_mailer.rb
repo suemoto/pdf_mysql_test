e7f269106e7900911e911e8b044c9635cb9e7cac
suemoto <s_bdash_0802@yahoo.co.jp>
2020-01-17 20:26:30 +0900

    Update application_mailer.rb
    送り主の記述を変更

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,10 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'jungle事務局'
+  layout 'mailer'
+
+
+  # default from:     "メールテスト運営局",
+  #         bcc:      "sample+sent@gmail.com",
+  #         reply_to: "sample+reply@gmail.com"
   layout 'mailer'
 end
