e54a06ddde9c1198d203fc961bdbdb323c83a669
suemoto <s_bdash_0802@yahoo.co.jp>
2020-01-17 21:01:04 +0900

    Update art_new_mailer.rb
    メーラーのメソッドの記述を修正

--- app/mailers/art_new_mailer.rb
+++ app/mailers/art_new_mailer.rb
@@ -5,9 +5,15 @@ class ArtNewMailer < ApplicationMailer
   #
   #   en.art_new_mailer.art_new_mailer.subject
   #
-  def art_new_mailer
-    @greeting = "Hi"
+  # def art_new_mailer
+  #   @greeting = "Hi"
 
-    mail to: "to@example.org"
+  #   mail to: "to@example.org"
+  # end
+  def art_new_mailer(article,user)
+    @article = article
+    @user = user
+    mail to:      user.email,
+         subject: '会員情報が更新されました。'
   end
 end
