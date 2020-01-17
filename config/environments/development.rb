883e5db0488a69bc31df1da27d68ac3cfbecaa07
suemoto <s_bdash_0802@yahoo.co.jp>
2020-01-17 14:46:37 +0900

    mail_opner_install
    開発環境でメールを送信したときに、ブラウザでメールを開くgemをインストール。

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,16 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  if Rails.env.production?
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      port:                 587,
+      address:              'smtp.gmail.com',
+      domain:               'smtp.gmail.com',
+      user_name:            '<YOUR EMAIL ADDRESS>',
+      password:             '<YOUR EMAIL PASSWORD>',
+      authentication:       'login',
+      enable_starttls_auto: true
+    }
 end
