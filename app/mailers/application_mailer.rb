1b4b354b86382fe6ace265d4581f60933db0bccd
suemoto <s_bdash_0802@yahoo.co.jp>
2020-01-17 20:51:08 +0900

    change_default_address
    メールアドレスを環境変数に変更

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,10 +1,10 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'jungle事務局'
-  layout 'mailer'
+  layout 'article_mailer_mailer/art_new_mailer'
 
 
   # default from:     "メールテスト運営局",
   #         bcc:      "sample+sent@gmail.com",
   #         reply_to: "sample+reply@gmail.com"
-  layout 'mailer'
+  # layout 'mailer'
 end
--- app/mailers/article_mailer.rb
+++ app/mailers/article_mailer.rb
@@ -1,11 +1,11 @@
 class ArticleMailer < ApplicationMailer
-  default from: 'n.s.bdash@gmail.com'
+  default from: ENV['MAILER_ADDRESS']
 
   def art_new_mailer(myfile,user)
     @myfile = myfile
     @user = user
 
-    mail to: user.email  #宛先
-         subject: "新規記事を作成しました", #メールのタイトル
+    mail to: user.email,  #宛先
+         subject: "新規記事を作成しました" #メールのタイトル
   end
 end
