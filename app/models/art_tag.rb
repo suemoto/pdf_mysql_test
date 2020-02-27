62201c7f55d02e440569b27e3cf959b1c6339f39
suemoto <s_bdash_0802@yahoo.co.jp>
2020-02-27 22:57:13 +0900

    Update art_tag.rb
    タグを追加。
    改善案をコメントアウトで記述

--- app/models/art_tag.rb
+++ app/models/art_tag.rb
@@ -1,5 +1,8 @@
 class ArtTag < ActiveHash::Base
   self.data = [
-      {id: 1, name: 'Ruby'}, {id: 2, name: 'Python'}
+      {id: 1, name: 'Ruby'}, {id: 2, name: 'Python'}, {id: 3, name: 'Git'}, {id: 4, name: 'JAVA'},{id: 5, name:'SQL'}, {id: 6, name: 'AWS'}
   ]
 end
+
+
+#配列に文字列を入れてeach文で取り出せばもう少し簡単な構造にできそう
